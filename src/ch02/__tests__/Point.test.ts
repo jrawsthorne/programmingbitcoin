9200455eb481f778d837e72ae44f68d3fd791fe1
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2019-07-17 00:01:04 +0100

    Add on curve test for Point

--- src/ch02/__tests__/Point.test.ts
+++ src/ch02/__tests__/Point.test.ts
@@ -6,6 +6,14 @@ test("equals", () => {
   expect(a.equals(b)).toBeFalsy();
 });
 
+test("on curve", () => {
+  expect(() => new Point({ x: -2, y: 4, a: 5, b: 7 })).toThrowError(
+    "(-2, 4) is not on the curve"
+  );
+  new Point({ x: 3, y: -7, a: 5, b: 7 });
+  new Point({ x: 18, y: 77, a: 5, b: 7 });
+});
+
 test("add", () => {
   let a = new Point({ a: 5, b: 7 });
   let b = new Point({ x: 2, y: 5, a: 5, b: 7 });
@@ -19,5 +27,5 @@ test("add", () => {
   expect(a.add(b).equals(new Point({ x: 2, y: -5, a: 5, b: 7 }))).toBeTruthy();
 
   a = new Point({ x: -1, y: -1, a: 5, b: 7 });
-  expect(a.add(a).equals(new Point({ x: 18, y: 77, a: 5, b: 7 })));
+  expect(a.add(a).equals(new Point({ x: 18, y: 77, a: 5, b: 7 }))).toBeTruthy();
 });
