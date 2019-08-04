74cb21e787c853254a7cfdc48ea1337019d00d02
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2019-08-04 01:16:56 +0100

    Add more SEC parse tests

--- src/ch04/__tests__/S256.test.ts
+++ src/ch04/__tests__/S256.test.ts
@@ -54,4 +54,28 @@ test("parse", () => {
   let point = G.rmul(coefficient);
   expect(S256Point.parse(uncompressed).equals(point)).toBe(true);
   expect(S256Point.parse(compressed).equals(point)).toBe(true);
+  coefficient = 123;
+  uncompressed = Buffer.from(
+    "04a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5204b5d6f84822c307e4b4a7140737aec23fc63b65b35f86a10026dbd2d864e6b",
+    "hex"
+  );
+  compressed = Buffer.from(
+    "03a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5",
+    "hex"
+  );
+  point = G.rmul(coefficient);
+  expect(S256Point.parse(uncompressed).equals(point)).toBe(true);
+  expect(S256Point.parse(compressed).equals(point)).toBe(true);
+  coefficient = 42424242;
+  uncompressed = Buffer.from(
+    "04aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e21ec53f40efac47ac1c5211b2123527e0e9b57ede790c4da1e72c91fb7da54a3",
+    "hex"
+  );
+  compressed = Buffer.from(
+    "03aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e",
+    "hex"
+  );
+  point = G.rmul(coefficient);
+  expect(S256Point.parse(uncompressed).equals(point)).toBe(true);
+  expect(S256Point.parse(compressed).equals(point)).toBe(true);
 });
