b46bf56c363fa1eae6cda383e807c6f000615800
Jake Rawsthorne <jake@jakerawsthorne.co.uk>
2019-08-09 00:20:00 +0100

    Add disk cache for TxFetcher

--- src/ch05/TxFetcher.ts
+++ src/ch05/TxFetcher.ts
@@ -1,8 +1,13 @@
 import { Tx } from "./Tx";
 import fetch from "node-fetch";
+import fs from "fs";
 
 interface TxCahe {
   [key: string]: Tx;
+}
+
+interface DiskCache {
+  [keyof: string]: string;
 }
 
 export class TxFetcher {
@@ -21,16 +26,17 @@ export class TxFetcher {
   ): Promise<Tx> => {
     if (fresh || TxFetcher.cache[txId] === undefined) {
       const url = `${TxFetcher.getUrl(testnet)}/tx/${txId}/hex`;
-      const response = await fetch(url).then(res => res.text());
-      let raw = Buffer.from(response, "hex");
+      let response = await fetch(url);
+      if (response.status !== 200) {
+        throw Error(`Error fetching tx: ${txId}`);
+      }
+      const rawTx = await response.text();
+      let raw = Buffer.from(rawTx, "hex");
       let tx: Tx;
       if (raw[4] === 0) {
         raw = Buffer.concat([raw.slice(0, 4), raw.slice(6)]);
         tx = Tx.parse(raw, testnet);
-        tx.locktime = raw
-          .reverse()
-          .slice(0, 4)
-          .readUInt32LE(0);
+        tx.locktime = raw.slice(raw.length - 4).readUInt32LE(0);
       } else {
         tx = Tx.parse(raw, testnet);
       }
@@ -40,5 +46,24 @@ export class TxFetcher {
     }
     TxFetcher.cache[txId].testnet = testnet;
     return TxFetcher.cache[txId];
+  };
+
+  static loadCache = (filename: string): void => {
+    const diskCache: DiskCache = JSON.parse(
+      fs.readFileSync(filename).toString("ascii")
+    );
+
+    Object.entries(diskCache).map(([k, rawHex]) => {
+      let raw = Buffer.from(rawHex, "hex");
+      let tx: Tx;
+      if (raw[4] === 0) {
+        raw = Buffer.concat([raw.slice(0, 4), raw.slice(6)]);
+        tx = Tx.parse(raw);
+        tx.locktime = raw.slice(raw.length - 4).readUInt32LE(0);
+      } else {
+        tx = Tx.parse(raw);
+      }
+      TxFetcher.cache[k] = tx;
+    });
   };
 }
